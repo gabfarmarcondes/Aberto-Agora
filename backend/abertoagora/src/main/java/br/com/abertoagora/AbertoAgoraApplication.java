a9be5eff516b6c0a067e1f65ad933497b05d7d0d
gabfarmarcondes <gabriel.fmarcondes@hotmail.com>
2025-03-05 23:52:43 -0300

    First Commit

---
+++ backend/abertoagora/src/main/java/br/com/abertoagora/AbertoAgoraApplication.java
@@ -0,0 +1,13 @@
+package br.com.abertoagora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbertoAgoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbertoAgoraApplication.class, args);
+	}
+
+}
---
+++ backend/abertoagora/src/test/java/br/com/abertoagora/AbertoAgoraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.abertoagora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbertoAgoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
